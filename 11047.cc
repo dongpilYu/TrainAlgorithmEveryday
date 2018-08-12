d2209406d6045e9863da84dbf330de175a6633d0
YUDONGPIL\yudongpil <yoodongphil@naver.com>
2018-08-12 18:19:01 +0900

    Add greedy algorithm example

--- 11047.cc
+++ 11047.cc
@@ -17,13 +17,20 @@ using namespace std;
 int main()
 {
     int n, k;
-    cin>>n>>k;
+    cin >> n >> k;
     vector<int> a(n);
-    for(int i=0;i<n;i++)
-    {
+    for (int i=0; i<n; i++)
         cin >> a[i];
-        
+    // n개의 동전의 가치를 입력
+    int ans = 0;
+    for (int i=n-1; i>=0; i--){
+        ans += k/a[i];
+        k %= a[i];
     }
+    cout << ans << '\n';
+    return 0;
+
+    
 }
 
 
