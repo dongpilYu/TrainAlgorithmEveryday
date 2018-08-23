e5bc09828a2883ddc13e5e3f360e08dfe09583ba
dong <yoo@naver.com>
2018-08-23 17:57:20 +0900

    Complete mergeSort

--- algorithm/mergeSort.cc
+++ algorithm/mergeSort.cc
@@ -12,6 +12,11 @@
 
     문제가 겹치지 않기 때문에 Memoization을 쓸 필요 없다.
 
+    탈출 조건은 두개 미만의 요소가 포함된 하위 배열(right<=left))
+    요소가 하나도 없거나 하나만 있는 하위 배열은 이미 정렬된 것
+    하나도 없을 수는 없다. 그러므로 right == left로 해도 무관할 듯
+
+    nlogn
 */
 #include <iostream>
 using namespace std;
@@ -31,28 +36,58 @@ void print_arr(int arr[], int size)
 
 void merge(int arr[], int left, int mid, int right)
 {
-    int i,j,k;
-    i = left;
-    j = mid + 1;
-    k = left;
+    // mid를 기준으로 [0,mid] [mid+1,end]로
+    // 배열이 나누어진 상태
+    int sorted_arr[SIZE];
+    int idx_left = left;
+    int idx_right = mid+1;
+    int idx = left;
 
-    int tmp_arr[SIZE];
+    // idx, idx_left, idx_right
+    // mid = 3, right = 6
+    // 0, 0, 4
+    // 1, 1, 4
+    // 2, 2, 4
+    // 3, 2, 5
+    // 4, 3, 5
+    // 5, 4, 5
+    // 6, 5, 6
+    // 7, 5, 7
+    while(idx_left <= mid && idx_right <= right)
+    {
+        if(arr[idx_left] > arr[idx_right])
+            sorted_arr[idx++] = arr[idx_right++];
+        else
+            sorted_arr[idx++] = arr[idx_left++];
+    }
+    if(idx_left > mid)
+        while(idx_right <= right)
+            sorted_arr[idx++] = arr[idx_right++];       
+    
+    else if(idx_right > right)
+        while(idx_left <= mid)
+            sorted_arr[idx++] = arr[idx_left++];
+   
+    for(int i=left;i<right+1;i++)
+        arr[i] = sorted_arr[i];
 }
 
 void merge_sort(int arr[], int left, int right)
 {
-
     if(left < right) // left == right 인 경우는 하나의 원소만 존재
     {
         int mid = (left + right) / 2;
-        merge_sort(arr, left, mid); // 정렬됨 
-        merge_sort(arr, mid + 1, right); // 정렬됨 
-        merge(arr, left, mid, right); // 합치기
+        // mid의 값을 계산해서 넘기나 넘겨서 계산하나 무관
+        merge_sort(arr, left, mid); 
+        merge_sort(arr, mid + 1, right); 
+        merge(arr, left, mid, right); 
+        // 두 배열을 합치는데, 두 배열은 모두 정렬된 상태
     }
 }
 int main()
 {
-    int arr[10] = {15, 2, 24, 18, 7, 13, 12, 4, 21, 9};
+    int arr[SIZE] = {15, 2, 24, 18, 7, 13, 12, 4, 21, 9};
+    //int arr[SIZE] = {1,3,7,11,4,15,17};
     print_arr(arr, SIZE);
     merge_sort(arr, START, END);
     print_arr(arr, SIZE);
